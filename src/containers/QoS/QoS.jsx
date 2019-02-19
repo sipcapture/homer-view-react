e9d37df753332ebdbbe20278724c07251e753fb4
almukhametovd <almukhametovd@gmail.com>
2019-02-19 10:25:28 +0200

    Data stats

--- src/containers/QoS/QoS.jsx
+++ src/containers/QoS/QoS.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { render } from "react-dom";
 import PropTypes from "prop-types";
 import { hot } from "react-hot-loader"
+
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -89,6 +91,23 @@ const baselineStyleExtraLite = {
   }
 };
 
+const statBlock = {
+  width: '200px',
+  height: '200px'
+};
+
+const statBlockTitle = {
+  textAlign: 'center',
+  padding: '16px',
+  fontSize: '20px'
+};
+
+const statBlockValue = {
+  textAlign: 'center',
+  padding: '50px',
+  fontSize: '24px'
+};
+
 class CrossHairs extends React.Component {
   render() {
     const { x, y } = this.props;
@@ -140,7 +159,8 @@ class QOS extends React.Component {
     });
   }
 
-  render() {
+
+  renderCharts() {
     const { _reports, _labels, reportData, _stats } = this.props.data;
     const { isPackets, isIaJitters, isHighestSeq, isOcters, isLsr } = this.state;
 
@@ -155,7 +175,7 @@ class QOS extends React.Component {
     if (reportData[0] && reportData[0].values) {
       reportData[0].values.forEach((data)=> {
         packetsPoints.push([data.x, data.y]);
-        if (data.y > maxValue) {
+        if (isPackets && data.y > maxValue) {
           maxValue = data.y;
         }
       });
@@ -164,7 +184,7 @@ class QOS extends React.Component {
     if (reportData[1] && reportData[1].values) {
       reportData[1].values.forEach((data)=> {
         octersPoints.push([data.x, data.y]);
-        if (data.y > maxValue) {
+        if (isOcters && data.y > maxValue) {
           maxValue = data.y;
         }
       });
@@ -173,7 +193,7 @@ class QOS extends React.Component {
     if (reportData[2] && reportData[2].values) {
       reportData[2].values.forEach((data)=> {
         highestSeqNoPoints.push([data.x, data.y]);
-        if (data.y > maxValue) {
+        if (isHighestSeq && data.y > maxValue) {
           maxValue = data.y;
         }
       });
@@ -182,7 +202,7 @@ class QOS extends React.Component {
     if (reportData[3] && reportData[3].values) {
       reportData[3].values.forEach((data)=> {
         IaJitterPoints.push([data.x, data.y]);
-        if (data.y > maxValue) {
+        if (isIaJitters && data.y > maxValue) {
           maxValue = data.y;
         }
       });
@@ -191,7 +211,7 @@ class QOS extends React.Component {
     if (reportData[4] && reportData[4].values) {
       reportData[4].values.forEach((data)=> {
         lsrPoints.push([data.x, data.y]);
-        if (data.y > maxValue) {
+        if (isLsr && data.y > maxValue) {
           maxValue = data.y;
         }
       });
@@ -227,97 +247,158 @@ class QOS extends React.Component {
       points: lsrPoints
     });
 
+    return (
+      <Resizable>
+        <ChartContainer
+          title="QoS"
+          titleStyle={{ fill: "#555", fontWeight: 500 }}
+          timeRange={packetsSeries.range()}
+          format="%H:%M:%S"
+          timeAxisTickCount={5}
+        >
+          <ChartRow height="300">
+            <YAxis
+              id="value"
+              label="Count"
+              min={0}
+              max={maxValue}
+              showGrid={true}
+              width="60"
+            />
+            <Charts>
+              <CrossHairs x={this.state.x} y={this.state.y} />
+              {isPackets
+                ? <LineChart axis="value" series={packetsSeries} style={style} />
+                : <Baseline axis="value" visible={false}/>}
+              {isOcters
+                ? <LineChart axis="value" series={octersSeries} style={style} />
+                : <Baseline axis="value" visible={false}/>}
+              {isHighestSeq
+                ? <LineChart axis="value" series={highestSeqNoSeries} style={style} />
+                : <Baseline axis="value" visible={false}/>}
+              {isIaJitters
+                ? <LineChart axis="value" series={IaJitterSeries} style={style} />
+                : <Baseline axis="value" visible={false}/>}
+              {isLsr
+                ? <LineChart axis="value" series={lsrSeries} style={style} />
+                : <Baseline axis="value" visible={false}/>}
+            </Charts>
+          </ChartRow>
+        </ChartContainer>
+      </Resizable>
+    );
+  }
+
+  renderForm() {
+    const { isPackets, isIaJitters, isHighestSeq, isOcters, isLsr } = this.state;
+
+    return (
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Graphs</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox checked={isPackets} onChange={this.handleInputChange} name="isPackets" />
+            }
+            label="packets"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={isOcters} onChange={this.handleInputChange} name="isOcters" />
+            }
+            label="octers"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={isHighestSeq} onChange={this.handleInputChange} name="isHighestSeq" />
+            }
+            label="highest seq"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={isIaJitters} onChange={this.handleInputChange} name="isIaJitters" />
+            }
+            label="ia jitters"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={isLsr} onChange={this.handleInputChange} name="isLsr" />
+            }
+            label="lsr"
+          />
+        </FormGroup>
+        <FormHelperText>Check graphs</FormHelperText>
+      </FormControl>
+    )
+  }
+
+  renderStats() {
+    const { _stats } = this.props.data;
+
+    const statsList = [];
+
+    for (let key in _stats) {
+      for (let statKey in _stats[key]) {
+        statsList.push({
+          parentKey: key,
+          key: statKey,
+          value: _stats[key][statKey]
+        })
+      }
+    }
+
+    const listForRender = statsList.map((stat) => {
+     return (
+       <Grid item xs>
+         <Paper style={statBlock}>
+           <div style={statBlockTitle}>
+             {`${stat.key} ${stat.parentKey.toUpperCase()}`}
+           </div>
+           <div style={statBlockValue}>
+             {`${stat.value}`}
+           </div>
+         </Paper>
+       </Grid>)
+    });
+
+    return listForRender;
+  }
+
+  render() {
+    const { _reports, _labels, reportData, _stats } = this.props.data;
+
+    console.log(_stats)
 
     return (
       <div
         style={chartContainer}
         className="chart-container">
 
-        <Card>
-          <CardContent>
-            <Resizable>
-              <ChartContainer
-                title="Package lost"
-                titleStyle={{ fill: "#555", fontWeight: 500 }}
-                timeRange={packetsSeries.range()}
-                format="'%H '%m '%S"
-                timeAxisTickCount={5}
-              >
-                <ChartRow height="150">
-                  <YAxis
-                    id="price"
-                    label="Count"
-                    min={0}
-                    max={maxValue}
-                    showGrid={true}
-                    width="60"
-                  />
-                  <Charts>
-                    <CrossHairs x={this.state.x} y={this.state.y} />
-                    {isPackets
-                      ? <LineChart axis="price" series={packetsSeries} style={style} />
-                      : <span></span>}
-                    {isOcters
-                      ? <LineChart axis="price" series={octersSeries} style={style} />
-                      : <span></span>}
-                    {isHighestSeq
-                      ? <LineChart axis="price" series={highestSeqNoSeries} style={style} />
-                      : <span></span>}
-                    {isIaJitters
-                      ? <LineChart axis="price" series={IaJitterSeries} style={style} />
-                      : <span></span>}
-                    {isLsr
-                      ? <LineChart axis="price" series={lsrSeries} style={style} />
-                      : <span></span>}
-                  </Charts>
-                </ChartRow>
-              </ChartContainer>
-            </Resizable>
-          </CardContent>
-        </Card>
+        <Grid container spacing={24}>
+          <Grid item xs>
 
-        <br/>
+            <Grid container spacing={24}>
+              {this.renderStats()}
+            </Grid>
 
-        <Card>
-          <CardContent>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">Graphs</FormLabel>
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Checkbox checked={isPackets} onChange={this.handleInputChange} name="isPackets" />
-                  }
-                  label="packets"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox checked={isOcters} onChange={this.handleInputChange} name="isOcters" />
-                  }
-                  label="octers"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox checked={isHighestSeq} onChange={this.handleInputChange} name="isHighestSeq" />
-                  }
-                  label="highest seq"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox checked={isIaJitters} onChange={this.handleInputChange} name="isIaJitters" />
-                  }
-                  label="ia jitters"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox checked={isLsr} onChange={this.handleInputChange} name="isLsr" />
-                  }
-                  label="lsr"
-                />
-              </FormGroup>
-              <FormHelperText>Check graphs</FormHelperText>
-            </FormControl>
-          </CardContent>
-        </Card>
+          </Grid>
+          <Grid item xs>
+            <Card>
+              <CardContent>
+                {this.renderCharts()}
+              </CardContent>
+            </Card>
+
+            <br/>
+
+            <Card>
+              <CardContent>
+                {this.renderForm()}
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+
       </div>
     );
   }
