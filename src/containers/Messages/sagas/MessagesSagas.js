56545fbe2e1fe5e9deca35a6b01bcf3afd560229
almukhametovd <almukhametovd@gmail.com>
2019-02-27 13:54:34 +0200

    request url params

--- src/containers/Messages/sagas/MessagesSagas.js
+++ src/containers/Messages/sagas/MessagesSagas.js
@@ -1,51 +1,12 @@
 import { takeEvery, put, call } from "redux-saga/effects";
 import { getMessagesAsync } from "../actions";
-
-import axios from "axios"
-
-function fetchMessages() {
-  const payload = {
-    "timestamp":{
-      "from":1550707199000,
-      "to":1550707799000
-    },
-    "param":{
-      "search":{
-        "1_call":{
-          "id":6145,
-          "callid":[
-            "8afhjq@127.0.0.1"
-          ],
-          "uuid":[
-
-          ]
-        }
-      },
-      "location":{
-
-      },
-      "transaction":{
-        "call":true,
-        "registration":false,
-        "rest":false
-      },
-      "id":{
-
-      },
-      "timezone":{
-        "value":-120,
-        "name":"Local"
-      }
-    }
-  }
-
-  return axios.post('/api/v3/call/transaction', payload);
-}
+import fetchData from "../../../utils/api";
 
 export function* getMessagesDataSaga() {
-
   try {
-    const response = yield call(fetchMessages);
+    const response = yield call(fetchData, {
+      url: "/api/v3/call/transaction"
+    });
     const message = response.data;
     yield put(getMessagesAsync.success(message));
   } catch (err) {
